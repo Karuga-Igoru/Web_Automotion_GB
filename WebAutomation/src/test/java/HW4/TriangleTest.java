4e34850d0bc08d02fc9a9cab78c45ee56e1f2c91
Igor <84844891+Karuga-Igoru@users.noreply.github.com>
2022-04-15 15:38:48 +0500

    HW4 corrected

--- WebAutomation/src/test/java/HW4/TriangleTest.java
+++ WebAutomation/src/test/java/HW4/TriangleTest.java
@@ -1,10 +1,16 @@
 package HW4;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TriangleTest {
+
+    //private static Logger logger = LoggerFactory.getLogger(TriangleTest.class);
 
     Triangle triangle1 = new Triangle (5, 6, 7);
     Triangle triangle2 = new Triangle (5, 6, 70);
@@ -12,14 +18,20 @@ public class TriangleTest {
     @Test
     void isFindingSquareWorks() throws TriangleException {
         assertNotNull(triangle1.findingTriangleSquare());
+
     }
 
     @Test
     void isSidesNotNull() {
+        boolean side1check = triangle1.getFirstSide() != 0;
+        //if (side1check == false) logger.;                 //logging, don't work
+        boolean side2check = triangle1.getSecondSide() != 0;
+        boolean side3check = triangle1.getThirdSide() != 0;
+
         assertAll(
-                () -> assertTrue(triangle1.getFirstSide() != 0),
-                () -> assertTrue(triangle1.getSecondSide() != 0),
-                () -> assertTrue(triangle1.getThirdSide() != 0));
+                () -> assertTrue( side1check),
+                () -> assertTrue(side2check),
+                () -> assertTrue(side3check));
     }
 
     @Test
@@ -32,10 +44,13 @@ public class TriangleTest {
 
     @Test
     void exceptionCatch(){
-        assertThrows(TriangleException.class, () -> triangle2.findingTriangleSquare(),
-                "Triangle with wrong sides throw it");
+        assertDoesNotThrow(() -> triangle1.findingTriangleSquare());
     }
 
+    /*@Test                             // uncomment for negative result
+    void exceptionCatchNegative(){
+        assertDoesNotThrow(() -> triangle2.findingTriangleSquare());
+    }*/
 
 
 }
